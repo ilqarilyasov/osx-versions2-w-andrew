0c2a1067853f3eacd7a985ccbca9fc6a2e37c425
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-09 16:10:54 -0600

    Initial Commit

---
+++ OSXVersions2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OSXVersions2
+//
+//  Created by Ilgar Ilyasov on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OSXVersions2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OSXVersions2
+//
+//  Created by Ilgar Ilyasov on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
